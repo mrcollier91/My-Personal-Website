23fbd6d10b13e7bb1cc925926126c52482e7b2b4
mrcollier91 <colliermatt91@gmail.com>
2022-05-02 08:57:57 -0500

    tried JS funtion for text input

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Pipeliner to Software Developer";
+h2.textContent = "";
 document.querySelector("body").appendChild(h2);
+
+var element = document.getElementById("questionBtn").addEventListener("click", function()){
+    alert("hello world")
+};
+element.onclick = function (answerQustion) {
+    alert("hello world")
+}
